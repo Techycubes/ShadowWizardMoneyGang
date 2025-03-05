c691f439cf86326451c2c3e8a640f07d78ea7382
Techycubes <alijawadparpia@gmail.com>
2025-03-05 13:18:29 -0500

    ADD
    ADDED SELECTION

--- Assets/Scripts/Selection menu.cs
+++ Assets/Scripts/Selection menu.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class Selectionmenu : MonoBehaviour
 {
-    string CarName;
+    public static string CarName = "default";
     // Start is called before the first frame update
     public void Select(int i) // i could be ignored if toggling is the goal
     {
-        if (CarName == "default")
-        {
-            CarName = "Car2";
-            Debug.Log("Selected Car2");
-        }
-        else
-        {
+        if(i==0){
             CarName = "default";
-            Debug.Log("Selected default");
+        }else if(i==1){
+            CarName = "Car2";
+        } else if(i==2){
+            CarName = "Car3";
+        }else{
+            CarName = "default";
         }
     }
 }
